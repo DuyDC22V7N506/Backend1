71db81a436ba99e034014bfffd07988c9b0d40db
nkduy <duydc22v7n506@vlvh.ctu.edu.vn>
2026-03-15 10:05:30 +0700

    Cai dat express chay thong bao xin chao

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = required('./app');
-const config = require('./app/config');
+const app = require("./app");
+const config = require("./app/config");
 
-const PORT=config.app.port;
+const PORT = config.app.port;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 
